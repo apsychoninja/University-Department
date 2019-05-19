23e7b36b529a5e380269b183d5214b90eb5ddf14
apsychoninja <bcs02131114@gmail.com>
2019-05-20 01:35:07 +0500

    20-05-2019

--- resources/views/layouts/sections/index/welcome.blade.php
+++ resources/views/layouts/sections/index/welcome.blade.php
@@ -12,9 +12,8 @@
 	        				@if(\Request::is('about'))
 	        					<?php echo  '<i class="fa fa-graduation-cap" aria-hidden="true"></i><h3>CERTIFIED TEACHERS</h3><p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia con- sequuntur magni dolores eos qui ratione </p>' ?>
         					@else
-        						<?php echo  '<h3><i class="fa fa-graduation-cap" aria-hidden="true"></i> LEARN COUSES first</h3><p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia con- sequuntur magni dolores eos qui ratione </p>'?>}
+        						<?php echo  '<h3><i class="fa fa-graduation-cap" aria-hidden="true"></i> LEARN COUSES first</h3><p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia con- sequuntur magni dolores eos qui ratione </p>'?>
 	        				@endif
-{{-- 	        				<h3><i class="fa fa-graduation-cap" aria-hidden="true"></i> LEARN COUSES first</h3> --}}
 	        				
 	        			</div>
 	        			<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 wow fadeInUp">
@@ -29,12 +28,28 @@
 	        			</div>
 	        			<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 wow fadeInUp">
 	        				@if(\Request::is('about'))
-	        					<?php echo '<i class="far fa-gem" aria-hidden="true"></i>
-	        				<h3>Everything you need</h3>
-	        				<p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia con- sequuntur magni dolores eos qui ratione </p>' ?>
+	        					<i class="far fa-gem" aria-hidden="true"></i>
+	        					<h3>Everything you need</h3>
+	        					<p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia con- sequuntur magni dolores eos qui ratione </p>
 	        				@else
-	        					<?php echo '<h3><i class="far fa-gem" aria-hidden="true"></i> Everything you need</h3>
-	        				<p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia con- sequuntur magni dolores eos qui ratione </p>' ?>
+	        					<style type="text/css">
+	        						i.far.fa-gem {
+									    width: 92px;
+									    height: 92px;
+										background: transparent; 
+									    border-radius: 50%;
+									    -moz-border-radius: 50%;
+									    -webkit-border-radius: 50%;
+									    -o-border-radius: 50%;
+									    -ms-border-radius: 50%;
+									     margin: 25px auto 34px auto !important; 
+									     line-height: 0px !important; 
+									    font-size: 36px;
+									    color: #cd2122;
+									}
+	        					</style>
+	        					<h3><i class="far fa-gem" aria-hidden="true"></i> Everything you need</h3>
+	        					<p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia con- sequuntur magni dolores eos qui ratione </p>
 	        				@endif
 	        			</div>
 	        		</div>
